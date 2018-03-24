f015cb2dbe6dc6349102bdf3b4ec5590d9955e4d
Fabulej <fabulej@gmail.com>
2018-03-24 14:17:26 +0100

    rebuild query to improve performance and group kegs by beer, tap and expiration_date

--- app/controllers/kegs_controller.rb
+++ app/controllers/kegs_controller.rb
@@ -1,6 +1,10 @@
 class KegsController < ApplicationController
   def index
-    @kegs = Keg.all
+    @kegs = Keg.select("kegs.*, beers.name, taps.number").includes(:beer, :tap).order('beers.name, expiration_date').group(:name, :expiration_date, :in_use, :number)
+    # Zapytanie grupuje duplikaty beczek
+    # Keg.find_by_sql("SELECT COUNT(*), kegs.*, beers.name FROM kegs INNER JOIN beers ON kegs.beer_id=beers.id GROUP BY beers.name, kegs.in_use, kegs.expiration_date ")
+
+
   end
 
   private
