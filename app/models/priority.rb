b31649d10fc700d058c1eaa5ff5f8d95d6105714
Fabulej <fabulej@gmail.com>
2018-04-11 23:34:08 +0200

    add priorities through tap edit view

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -3,7 +3,6 @@ class Priority < ApplicationRecord
   belongs_to :beer, optional: true
   belongs_to :style, optional: true
   belongs_to :tap
-  validates_presence_of :wanted, default: true
 
   enum p_type: {same_beer: 0, p_beer: 1, p_brewery: 2, p_style: 3}
 
