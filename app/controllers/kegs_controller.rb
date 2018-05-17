42ead38a81c834354c38b0a0248f717ef31fa916
Fabulej <fabulej@gmail.com>
2018-05-17 15:02:55 +0200

    add new keg view, create multiple kegs at once

--- app/controllers/kegs_controller.rb
+++ app/controllers/kegs_controller.rb
@@ -3,13 +3,26 @@ class KegsController < ApplicationController
     @kegs = Keg.select("kegs.*, beers.name, taps.number").includes(:beer, :tap).order('beers.name, expiration_date').group(:name, :expiration_date, :in_use, :number)
     # Zapytanie grupuje duplikaty beczek
     # Keg.find_by_sql("SELECT COUNT(*), kegs.*, beers.name FROM kegs INNER JOIN beers ON kegs.beer_id=beers.id GROUP BY beers.name, kegs.in_use, kegs.expiration_date ")
-
-
   end
+
+  def create
+     respond_to do |format|
+       if Keg.new(kegs_params).valid?
+         params[:number].to_i.times do
+           Keg.create(kegs_params)
+         end
+         format.html { redirect_to kegs_path, notice: 'Keg was successfully created.' }
+         format.json { render :index, status: :created, location: @keg }
+       else
+         format.html { render :new }
+         format.json { render json: @keg.errors, status: :unprocessable_entity }
+       end
+     end
+   end
 
   private
 
   def kegs_params
-    params.require(:kegs).permit(:expiration_date, :in_use, :priority)
+    params.require(:keg).permit(:expiration_date, :beer_id, :bibi)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :priorities
   root to: "taps#index"
   devise_for :users
-  resources :kegs, only: :index
+  resources :kegs
   resources :beers
   resources :taps
 end
